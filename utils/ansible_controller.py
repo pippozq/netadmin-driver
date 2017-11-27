c48f886abb443f3f43f45cae32ef6fe5f61d68f1
pippozq <zengqi0529@163.com>
2017-11-27 20:08:37 +0800

    update conf

--- utils/ansible_controller.py
+++ utils/ansible_controller.py
@@ -2,18 +2,21 @@ from __future__ import print_function
 
 from tornado.web import RequestHandler
 from tornado.gen import coroutine
+from tornado.options import define, options
 from tornado.escape import json_decode
 from concurrent.futures import ThreadPoolExecutor
 
 from utils.ansible_task import AnsibleTask
 
 import sys
+
 sys.path.append('.')
+
+define("thread_count", help='thread count', type=int)
 
 
 class AnsibleController(RequestHandler):
-    executor = ThreadPoolExecutor(30)
-
+    executor = ThreadPoolExecutor(options.thread_count)
     vars = dict()
     user = dict()
 
@@ -42,6 +45,7 @@ class AnsibleController(RequestHandler):
         else:
             result_detail_dict = play.get_result()
             for key in result_detail_dict.keys():
-                result_detail_dict[key] = result_detail_dict[key].replace('\\n', '').replace('\\r', '').replace('\\t', '')
+                result_detail_dict[key] = result_detail_dict[key].replace('\\n', '').replace('\\r', '').replace('\\t',
+                                                                                                                '')
                 result_detail_dict[key] = json_decode(result_detail_dict[key])
             return self.return_json(code, result_detail_dict)
